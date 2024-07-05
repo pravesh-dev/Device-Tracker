c6f7af5428a4800ca9de29d704ffc588510e2b11
pravesh dav <humantorch1932@gmail.com>
2024-07-05 21:45:43 +0530

    set view engine and createdd folder structure public

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express()
 
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 
 const server = http.createServer(app);
 const io = socketIo(server);
+
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     res.send('hey')
